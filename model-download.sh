1994ca88a1ba74e194c7e0eae87814037bdddb79
mt-ito <mt-ito@infiniteloop.co.jp>
2026-06-04 15:05:14 +0900

    download local model

---
+++ model-download.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+mkdir models
+#wget -O models/Kimi-VL-A3B-Thinking-2506-Q8_0.gguf https://huggingface.co/ggml-org/Kimi-VL-A3B-Thinking-2506-GGUF/resolve/main/Kimi-VL-A3B-Thinking-2506-Q8_0.gguf?download=true
+#wget -O models/mmproj-Kimi-VL-A3B-Thinking-2506-Q8_0.gguf https://huggingface.co/ggml-org/Kimi-VL-A3B-Thinking-2506-GGUF/resolve/main/mmproj-Kimi-VL-A3B-Thinking-2506-Q8_0.gguf?download=true
+wget -O models/Ministral-3-3B-Instruct-2512-Q8_0.gguf https://huggingface.co/ggml-org/Ministral-3-3B-Instruct-2512-GGUF/resolve/main/Ministral-3-3B-Instruct-2512-Q8_0.gguf?download=true
+wget -O models/mmproj-Ministral-3-3B-Instruct-2512-Q8_0.gguf https://huggingface.co/ggml-org/Ministral-3-3B-Instruct-2512-GGUF/resolve/main/mmproj-Ministral-3-3B-Instruct-2512-Q8_0.gguf?download=true
